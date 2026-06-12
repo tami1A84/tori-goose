699d2d1d06c948c723f773a68e0f33f8e0231f1c
N <123137351+tami1A84@users.noreply.github.com>
2026-06-13 03:52:27 +0900

    i18n: add Japanese locale support

--- ui/desktop/src/i18n/i18n.test.ts
+++ ui/desktop/src/i18n/i18n.test.ts
@@ -73,6 +73,18 @@ describe('getLocale', () => {
     expect(getLocale()).toEqual({ locale: 'tr', messageLocale: 'tr' });
   });
 
+
+  it('supports Japanese from navigator.languages', () => {
+    vi.stubGlobal('navigator', { languages: ['ja-JP'] });
+    expect(getLocale()).toEqual({ locale: 'ja-JP', messageLocale: 'ja' });
+  });
+
+  it('supports POSIX-style Japanese locale from GOOSE_LOCALE', () => {
+    mockAppConfig({ GOOSE_LOCALE: 'ja_JP' });
+    vi.stubGlobal('navigator', { languages: ['xx-XX'] });
+    expect(getLocale()).toEqual({ locale: 'ja-JP', messageLocale: 'ja' });
+  });
+
   it('supports Hindi from navigator.languages', () => {
     vi.stubGlobal('navigator', { languages: ['hi-IN'] });
     expect(getLocale()).toEqual({ locale: 'hi-IN', messageLocale: 'hi' });
